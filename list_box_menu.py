bcb2ced5c4490befb65a3e0f14ba6419a567a4ae
oyewalemarvellous <marvellousoyewale64@gmail.com>
2026-04-21 17:09:13 +0100

    Add files via upload

--- list_box_menu.py
+++ list_box_menu.py
@@ -13,14 +13,21 @@ def remove_item():
     selection = list.curselection()
     if selection:
         list.delete(selection[0])
+
+def remove_all_item():
+    list.delete(0,END)
+
 box= Entry(window)
 box.place(x= 10,y= 10)
 
-add= Button(window, text= "add", width = 10,command= add_item)
+add= Button(window, text= "add", width = 5,command= add_item)
 add.place(x= 150, y= 10)
 
-remove= Button(window, text ="remove", width= 10, command = remove_item)
-remove.place(x= 250, y= 10)
+remove= Button(window, text ="remove", width= 5, command = remove_item)
+remove.place(x= 200, y= 10)
+
+all = Button(window, text= "remove all", width = 10, command = remove_all_item)
+all.place(x= 250 ,y = 10)
 
 list = Listbox(window, height= 20, width= 50, bg ="sky blue")
 list.place(x= 10, y= 50)
